81c62e020b34d9a3274ed1b1d06bf136c0fb1d3f
didi <didier.mendoza.uni@gmail.com>
2023-11-26 04:08:11 -0600

    cards listas

---
+++ src/db/db.js
@@ -0,0 +1,62 @@
+const Algo= {
+
+    Data : [
+    
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver',
+        Logo: ''
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    {
+        img: 'https://www.stcatherines.net.au/wp-content/uploads/2017/03/algorithm.png',
+        Salon: 'PRIMERO A',
+        Boton: 'Ver'
+    },
+    
+    
+    ]
+    
+    }
+    
+    export default Algo;
